a037c8b816bd00c738e110ac50269980687dc20a
coursera-ws <wei.sarah.sun@gmail.com>
2015-03-06 17:44:10 -0800

    Update Plot1.r

--- Plot1.r
+++ Plot1.r
@@ -1,7 +1,7 @@
 ##Read data and create datetime variable
 data<-read.table("household_power_consumption.txt",sep=";", na.strings="?",header=TRUE)
 dt<-data[as.Date(data$Date,"%d/%m/%Y") %in% as.Date(c("2007-02-01","2007-02-02"),"%Y-%m-%d"), ]
-datetime<-strptime(paste(dt$Date,dt$Time),"%d/%m/%Y %H:%M:%S")
+dt$datetime<-strptime(paste(dt$Date,dt$Time),"%d/%m/%Y %H:%M:%S")
 
 ##Plot1
 png(file="Plot1.png")
